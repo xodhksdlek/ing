f177b6a6775e554378e31f0f7f608a9cac3ada8a
1103-18 <1103-18@1103-18-PC>
2018-03-09 16:46:31 +0900

    가입 작업 시작

--- WebContent/WEB-INF/view/index.jsp
+++ WebContent/WEB-INF/view/index.jsp
@@ -5,16 +5,25 @@
 <head>
 <title>Spring - managed by GIT</title>
 </head>
+<style>
+	a {
+		text-decoration: none;
+	}
+	
+</style>
 <body>
 	<div align="center">
 		<div style="width: 980px;">
 			<div>
 				<h1>Spring Project</h1>
 				<small>- ${ment } -</small>
-				<hr>
 			</div>
-			<div>
+			<hr/>
+			<div align="right" style="padding-right: 20px;">
+				<a href="/login"><span>Sign in</span></a> <span>or</span> 
+				<a href="/join"><span>Sign up</span></a>
 			</div>
+			<hr/>
 		</div>
 	</div>
 </body>
---
+++ src/total/controller/JoinController.java
@@ -0,0 +1,22 @@
+package total.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import total.service.GreetService;
+
+@Controller
+@RequestMapping("/join")
+public class JoinController {
+
+	@Autowired
+	GreetService greetService;
+	
+	@RequestMapping("/join")
+	public String joinHandle(Model model) {
+		model.addAttribute("ment", greetService.make());
+		return "join";
+	}
+}
